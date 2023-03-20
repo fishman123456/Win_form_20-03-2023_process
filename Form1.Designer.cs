5ec432937b56208f5da45b158e4e0f41f0ce044f
denis <106389581+fishman123456@users.noreply.github.com>
2023-03-20 19:53:28 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Win_form_20_03_2023_process
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Process = new System.Diagnostics.Process();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Process
+            // 
+            this.Process.StartInfo.Domain = "";
+            this.Process.StartInfo.LoadUserProfile = false;
+            this.Process.StartInfo.Password = null;
+            this.Process.StartInfo.StandardErrorEncoding = null;
+            this.Process.StartInfo.StandardOutputEncoding = null;
+            this.Process.StartInfo.UserName = "";
+            this.Process.SynchronizingObject = this;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(113, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 58);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 263);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "U-83";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Diagnostics.Process Process;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Win_form_20_03_2023_process
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+                InitializeComponent();
+                Process.StartInfo = new System.Diagnostics.
+                ProcessStartInfo("calc.exe");
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.CloseMainWindow();
+            Process.Close();
+        }
+
+       
+    }
+}
